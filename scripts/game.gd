27752c0e575072b657ac11f9691ed8ce7089e127
paajnW <mufeed.talukder555@gmail.com>
2024-10-14 20:26:10 +0600

    added obstacle and randomness upto 36:10 before difficulty

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,22 +1,31 @@
 extends Node2D
 
+#preload obstacles
+var heli_scene = preload("res://scenes/heli.tscn")
+var army_scene = preload("res://scenes/army.tscn")
+var obstacle_type := [army_scene]
+var obstacles:Array
+var heli_heights :=[200,390]
 
 #game variable
 const PLAYER_START_POS:=Vector2i(83,587)
 const CAM_START_POS:=Vector2i(579,332)
-
-var speed : float
-var score: int
 const START_SPEED:float =10.0
 const SCORE_MODIFIER: int =10
 const MAX_SPEED: int =25
+const SPEED_MODIFIER: int =5000
+
+var speed : float
+var score: int
 var screen_size : Vector2i
 var game_running: bool
-const SPEED_MODIFIER: int =5000
+var ground_height:int
+var last_obs
 
 func _ready():
 	screen_size=get_window().size
-	pass
+	#ground_height=$Platform.texture.get_height()
+	new_game()
 	
 func new_game():
 	#reset the nodes
@@ -33,6 +42,9 @@ func _process(delta):
 		speed=START_SPEED+  score/SPEED_MODIFIER
 		if speed> MAX_SPEED:
 			speed=MAX_SPEED
+		
+		#generate obstacles
+		generate_obs()
 		show_score()
 		if Input.is_action_pressed("ui_right"):
 			$player.position.x +=speed
@@ -54,3 +66,22 @@ func _process(delta):
 func show_score():
 	$HUD.get_node("Score").text="SCORE:"+str(score/SCORE_MODIFIER)
 	
+	
+func generate_obs():
+	#generate ground obstacles
+	if obstacles.is_empty() or last_obs.position.x<score+randi_range(90,100):
+		var obs_type = obstacle_type[randi() % obstacle_type.size()]
+		var obs
+		var max_obs = 3
+		for i in range(randi() % max_obs +1):
+			obs = obs_type.instantiate()
+			var obs_height = obs.get_node("Sprite2D").texture.get_height()
+			var obs_scale = obs.get_node("Sprite2D").scale
+			var obs_x:int = screen_size.x + score + 100
+			var obs_y:int = screen_size.y -ground_height - (obs_height*obs_scale.y/2)-5
+			last_obs = obs
+			add_obs(obs,obs_x,obs_y)
+func add_obs(obs,x,y):
+	obs.position = Vector2i(x,y)
+	add_child(obs)
+	obstacles.append(obs)
